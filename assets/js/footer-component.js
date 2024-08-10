68cf739524ff760eccb5f3fdf0a93ff05bf94b71
jfowler-dc <jorofodesign@gmail.com>
2024-08-10 19:50:14 -0400

    added footer component. updated color again

---
+++ assets/js/footer-component.js
@@ -0,0 +1,21 @@
+class FooterComponent extends HTMLElement {
+    constructor() {
+        super();
+
+        // Create wrapper
+        this.wrapper = document.createElement('footer');
+        const date = new Date();
+        this.year = date.getFullYear();
+        // Create the template
+        this.wrapper.innerHTML = `
+            <div class="wrap pb-6">
+                <span class="color-gray"><a href="/">jorofo.com</a> | ©${this.year}</span>
+            </div>
+        `;
+
+        // Attach the wrapper to the element (not shadow DOM)
+        this.appendChild(this.wrapper);
+    }
+}
+
+customElements.define('footer-component', FooterComponent);
